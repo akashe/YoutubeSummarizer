6c8ea1eb7ef3b58e6ecc8dc61efd9b5460c0ea6d
Akash Kumar <akashkumar@Akashs-MacBook-Air.local>
2024-01-14 21:28:39 +0530

    for testing by public

--- youtube_summarizer/pages/Summarize_youtube_channels.py
+++ youtube_summarizer/pages/Summarize_youtube_channels.py
@@ -38,6 +38,7 @@ st.markdown(
 )
 ui_spacer(2)
 
+'''
 with st.expander("Configuration"):
     model_name = st.selectbox(
         'Which LLM you prefer to use?',
@@ -47,6 +48,9 @@ with st.expander("Configuration"):
 
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+'''
+model_name = 'GPT-4-1106-Preview: Precise but costly'.split(":")[0].lower()
+openai_api_key = st.secrets("openai_api_key")
 
 with st.form("YoutubeSummary"):
     try:
