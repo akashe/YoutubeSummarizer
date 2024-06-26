ba804277a1ff88b424113474c50c21e25c69dfe8
akashe <infiniteakashe@gmail.com>
2024-06-27 00:42:22 +0530

    adjusting daily community dollars

--- youtube_summarizer/chatbot.py
+++ youtube_summarizer/chatbot.py
@@ -30,7 +30,7 @@ from function_definitions import function_definitions
 
 from db import *
 
-daily_community_dollars = 5
+daily_community_dollars = 3.5
 
 # Initialize the db if not done yet
 create_db_and_table()
