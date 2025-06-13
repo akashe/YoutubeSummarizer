d1eb0c4ca386f928bc63f83dacb30264a9eb0daf
akashe <infiniteakashe@gmail.com>
2025-06-14 00:15:07 +0530

    Updating port for rotating proxy IPs - 2

--- youtube_summarizer/process_single_transcript.py
+++ youtube_summarizer/process_single_transcript.py
@@ -37,7 +37,8 @@ def process_single_transcript(video_url: str,
         password = st.secrets["proxy_password"]
 
         ports = ["10001", "10002", "10003", "10004", "10005", "10006", "10007", "10008", "10009", "10010"]
-        port = random.choice(ports)
+        # port = random.choice(ports)
+        port = 7000
 
         proxy = f"http://{username}:{password}@gate.decodo.com:{port}"
         logger.info(f'proxy: {proxy}')
